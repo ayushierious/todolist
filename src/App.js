1ac5d5610561e52dba9cb835ceebe89ae4801952
ayushierious <ayushierious>
2023-04-01 03:15:36 +0530

    useState.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from "./MyComponents/Header";
 import Footer from "./MyComponents/Footer";
 import Todos from "./MyComponents/Todos";
-
+import React, {useState} from 'react';
 
 function App() {
+  const onDelete=(todo)=>{
+console.log("I am on delete" , todo);
+  }
+  const [todos, setTodos] = useState([
+    {
+    sno:1,
+    title: "Go to market",
+    desc: "you need to buy things"
+  },
+  {
+    sno:2,
+    title: "Go to bank",
+    desc: "you need to deposit money"
+  },
+  {
+    sno:3,
+    title: "Go to park",
+    desc: "you need to walk"
+  }
+    
+
+  ])
   return (
     <>
-  <Header title="MyTodosList"/>
+  <Header title="MyTodosList" searchBar={false}/>
+  
+  <Todos todos={todos} onDelete={onDelete}/> 
+
   <Footer/>
-  <Todos/>
  
     </>
   );
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
-export const Todos = () => {
+import {TodoItem} from "../MyComponents/TodoItem";
+export const Todos = (props) => {
   return (
-    <div>
-      Todos works.
+    <div className='container'>
+        <h3 className='text-center my-3'>Todos List</h3>
+     {props.todos.map((todo)=>{
+      return <TodoItem todo={todo} onDelete={props.onDelete}/>
+  })}
+     
     </div>
   )
 }
